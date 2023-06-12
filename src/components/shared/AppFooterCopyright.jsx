11a21cec5c5133971cb8b1fd5ba591a467c18a5d
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-06-12 22:07:29 +0530

    12-03-2023

---
+++ src/components/shared/AppFooterCopyright.jsx
@@ -0,0 +1,26 @@
+function AppFooterCopyright() {
+	return (
+		<div className="font-general-regular flex justify-center items-center text-center">
+			<div className="text-lg text-ternary-dark dark:text-ternary-light">
+				&copy; {new Date().getFullYear()}
+				<a
+					href="/"
+					target="__blank"
+					className="dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					Built by
+				</a>
+				{/* - */}
+				<a
+					href="https://github.com/RishabhJain-11/"
+					target="__blank"
+					className="text-secondary-dark dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					Rishabh Jain
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export default AppFooterCopyright;
---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Hello, my name is Rishabh Jain, and I am a passionate student of Information Technology currently in my 3rd year of studies. With a keen interest in web development, I have honed my skills to become a proficient Full-Stack Web Developer. Through my journey, I have gained expertise in various technologies and frameworks, enabling me to create robust and dynamic websites that deliver exceptional user experiences. Whether it\'s crafting elegant front- end interfaces or designing efficient back - end systems, I thrive on the challenges of building scalable and interactive web applications.',
+	},
+	{
+		id: 2,
+		bio: 'In addition to my web development skills, I am also actively engaged in learning Machine Learning. Fascinated by the potential of this cutting-edge technology, I am dedicated to expanding my knowledge in this field. I am exploring various algorithms and techniques, working on projects that involve data analysis, predictive modeling, and pattern recognition. By combining my expertise in Full-Stack Web Development with my growing proficiency in Machine Learning, I aspire to create intelligent and innovative solutions that can revolutionize the digital landscape. With a strong foundation in Information Technology and an insatiable curiosity for learning, I am excited about the endless possibilities that lie ahead in my journey as a tech enthusiast.',
+	},
+];
